6c2aa0f9d125fb5cda0471c8666e29cc198c6303
Eris Ristemena <e.ristemena@gmail.com>
2021-06-24 18:24:04 +0700

    Create joke service

---
+++ src/main/java/com/ngoprekweb/spring5jokesappv2/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.ngoprekweb.spring5jokesappv2.service;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/ngoprekweb/spring5jokesappv2/service/impl/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ngoprekweb.spring5jokesappv2.service.impl;
+
+import com.ngoprekweb.spring5jokesappv2.service.JokeService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
